73afb4a9840c67a7d04aff6c3ae9462ef3319be2
jonesne92 <neil.jones@prezi.com>
2022-08-11 10:56:57 +0100

    Log DB info

--- src/node/db/DB.js
+++ src/node/db/DB.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * The DB Module provides a database initialized with the settings
@@ -21,15 +21,22 @@
  * limitations under the License.
  */
 
-const ueberDB = require('ueberdb2');
-const settings = require('../utils/Settings');
-const log4js = require('log4js');
-const stats = require('../stats');
-const util = require('util');
+const ueberDB = require("ueberdb2");
+const settings = require("../utils/Settings");
+const log4js = require("log4js");
+const stats = require("../stats");
+const util = require("util");
+
+console.log("DB SETTINGS:");
+console.log(settings.dbSettings);
 
 // set database settings
-const db =
-    new ueberDB.Database(settings.dbType, settings.dbSettings, null, log4js.getLogger('ueberDB'));
+const db = new ueberDB.Database(
+  settings.dbType,
+  settings.dbSettings,
+  null,
+  log4js.getLogger("ueberDB")
+);
 
 /**
  * The UeberDB Object that provides the database functions
@@ -40,47 +47,48 @@ exports.db = null;
  * Initializes the database with the settings provided by the settings module
  * @param {Function} callback
  */
-exports.init = async () => await new Promise((resolve, reject) => {
-  db.init((err) => {
-    if (err) {
-      // there was an error while initializing the database, output it and stop
-      console.error('ERROR: Problem while initalizing the database');
-      console.error(err.stack ? err.stack : err);
-      process.exit(1);
-    }
-
-    if (db.metrics != null) {
-      for (const [metric, value] of Object.entries(db.metrics)) {
-        if (typeof value !== 'number') continue;
-        stats.gauge(`ueberdb_${metric}`, () => db.metrics[metric]);
+exports.init = async () =>
+  await new Promise((resolve, reject) => {
+    db.init((err) => {
+      if (err) {
+        // there was an error while initializing the database, output it and stop
+        console.error("ERROR: Problem while initalizing the database");
+        console.error(err.stack ? err.stack : err);
+        process.exit(1);
       }
-    }
 
-    // everything ok, set up Promise-based methods
-    ['get', 'set', 'findKeys', 'getSub', 'setSub', 'remove'].forEach((fn) => {
-      exports[fn] = util.promisify(db[fn].bind(db));
+      if (db.metrics != null) {
+        for (const [metric, value] of Object.entries(db.metrics)) {
+          if (typeof value !== "number") continue;
+          stats.gauge(`ueberdb_${metric}`, () => db.metrics[metric]);
+        }
+      }
+
+      // everything ok, set up Promise-based methods
+      ["get", "set", "findKeys", "getSub", "setSub", "remove"].forEach((fn) => {
+        exports[fn] = util.promisify(db[fn].bind(db));
+      });
+
+      // set up wrappers for get and getSub that can't return "undefined"
+      const get = exports.get;
+      exports.get = async (key) => {
+        const result = await get(key);
+        return result === undefined ? null : result;
+      };
+
+      const getSub = exports.getSub;
+      exports.getSub = async (key, sub) => {
+        const result = await getSub(key, sub);
+        return result === undefined ? null : result;
+      };
+
+      // exposed for those callers that need the underlying raw API
+      exports.db = db;
+      resolve();
     });
-
-    // set up wrappers for get and getSub that can't return "undefined"
-    const get = exports.get;
-    exports.get = async (key) => {
-      const result = await get(key);
-      return (result === undefined) ? null : result;
-    };
-
-    const getSub = exports.getSub;
-    exports.getSub = async (key, sub) => {
-      const result = await getSub(key, sub);
-      return (result === undefined) ? null : result;
-    };
-
-    // exposed for those callers that need the underlying raw API
-    exports.db = db;
-    resolve();
   });
-});
 
 exports.shutdown = async (hookName, context) => {
   await util.promisify(db.close.bind(db))();
-  console.log('Database closed');
+  console.log("Database closed");
 };
